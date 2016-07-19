1c1d43cb0ac917f243342479ce200f930bbf37ad
Neil Agarwal <nagar001@ucr.edu>
2016-07-18 17:07:29 -0700

    [SetUp] SetUp for AngularJS App

--- server/server.js
+++ server/server.js
@@ -2,10 +2,8 @@ var express = require('express');
 var app = express();
 var routes = require('./routes/index.js');
 
-app.use('/', routes);
+// app.use('/', routes);
+app.use(express.static(__dirname+'client'));
+app.listen(8080);
 
-app.set('port', process.env.PORT || 1738);
-
-var server = app.listen(app.get('port'), function() {
-  console.log('*Server Initiated*');
-});
+console.log('*Server Initiated*');
