9922d2a107757f7c67145e1f6ef0340783a4c71d
Neil Agarwal <nagar001@ucr.edu>
2016-07-20 17:18:42 -0700

    [SetUp] Current Controllers & Factories Needed

--- public/dist/built.js
+++ public/dist/built.js
@@ -507,42 +507,6 @@ angular
 
 	}
 
-(function() {
-
-angular
-  .module('app')
-  .factory('consumersListenersFactory', consumersListenersFactory);
-
-  consumersListenersFactory.$inject = ['$state', 'socketFactory'];
-
-  function consumersListenersFactory($state, socketFactory) {
-
-    var socket = socketFactory;
-
-    return {
-      init: init
-    };
-
-    // -----------------------
-    // consumers listeners
-
-    function init() {
-      if (!socket.isConnected()) {
-        console.error('*Socket NOT Connected. consumersListeners SetUp Failed*');
-        return;
-      }
-
-      socket.on('you are', youAre);
-    }
-
-    function youAre(resp) {
-      // event: 'you are'
-    }
-
-  }
-
-})();
-
 (function () {
 'use strict';
  angular
